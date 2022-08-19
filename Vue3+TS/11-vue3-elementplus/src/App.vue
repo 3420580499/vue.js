32d030b30774faa8eb16dc71a4ac59eeb0997de2
coderstr <3420580499@qq.com>
2022-08-19 10:32:56 +0800

    111

---
+++ Vue3+TS/11-vue3-elementplus/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <el-button>哈哈</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style lang="less"></style>
